39f83c0111b940857715622fac851e44fc3b4b93
Doc1998 <docmp98@gmail.com>
2026-08-04 15:09:54 +0100

    updates

---
+++ src/_data/client.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "name": "Kilkenny County Painters",
+    "email": "kilkennycountypainter@gmail.com",
+    "phoneForTel": "+353873566934",
+    "phoneFormatted": "+353873566934",
+    "address": {
+        "lineOne": "Kilkenny",
+        "lineTwo": "",
+        "city": "Kilkenny",
+        "state": "Kilkenny",
+        "zip": "",
+        "country": "IE",
+        "mapLink": ""
+    },
+    "socials": {
+        "facebook": "https://www.facebook.com/profile.php?id=61592374839970",
+        "instagram": "instagram.com"
+    },
+    "domain": "kilkennycountypainters.ie"
+};
